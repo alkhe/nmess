44351a4ae8a6348c89f2ff78b2130f91cd00cc09
chronize <sort@programmer.net>
2014-08-30 00:38:17 -0400

    initial stable

---
+++ mess.js
@@ -0,0 +1,39 @@
+module.exports = function(port) {
+
+    var express = require('express'),
+        app = express(),
+        http = require('http').createServer(app),
+        io = require('socket.io')(http),
+        path = require('path'),
+        stylus = require('stylus'),
+        compression = require('compression'),
+        session = require('express-session'),
+        logger = require('morgan')('dev'),
+        httpcodes = require('./utils/httpcodes');
+
+    var index = require('./routes/index')(io);
+
+    app.set('port', port)
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'jade');
+
+    app.use(logger)
+    .use(session({
+        secret: "{{secret}}",
+        resave: true,
+        saveUninitialized: true
+    }))
+    .use(stylus.middleware('./public'))
+    .use(express.static('./public'))
+    .use(compression());
+
+    app.use('/', index);
+
+    app.use(function(req, res) {
+        res.render('error', {
+            err: httpcodes[404]
+        });
+    });
+
+    return http;
+}
