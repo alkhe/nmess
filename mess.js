b3d8a7f1bc6c447189089a31fa2bf0323bbd0711
chronize <sort@programmer.net>
2014-09-24 02:28:24 -0400

    clear up, allow Socket.IO API

--- mess.js
+++ mess.js
@@ -16,35 +16,38 @@ module.exports = function(port, secret) {
         httpstatus = require('./utils/httpstatus');
 
     var index = require('./routes/index')(io),
-        api = require('./routes/api');
+        api = require('./routes/api')(io);
 
-    app.set('port', port)
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'jade');
+    app
+        .set('port', port)
+        .set('views', path.join(__dirname, 'views'))
+        .set('view engine', 'jade');
 
     app.use(logger)
-    .use(favicon('./public/favicon.ico'))
-    .use(session({
-        secret: secret,
-        resave: true,
-        saveUninitialized: true
-    }))
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({
-        extended: false
-    }))
-    .use(stylus.middleware('./public'))
-    .use(express.static('./public'))
-    .use(compression());
+        .use(favicon('./public/favicon.ico'))
+        .use(session({
+            secret: secret,
+            resave: true,
+            saveUninitialized: true
+        }))
+        .use(bodyParser.json())
+        .use(bodyParser.urlencoded({
+            extended: false
+        }))
+        .use(stylus.middleware('./public'))
+        .use(compression())
+        .use(express.static('./public'))
 
-    app.use('/', index)
-    .use('/api', api);
 
-    app.use(function(req, res) {
-        res.render('error', {
-            err: httpstatus[404]
+        .use('/', index)
+        .use('/api', api)
+
+
+        .use(function(req, res) {
+            res.render('error', {
+                err: httpstatus[404]
+            });
         });
-    });
 
     return http;
 }
--- routes/api.js
+++ routes/api.js
@@ -1,89 +1,92 @@
-var express = require('express'),
-	router = express.Router(),
-	control = require('../utils/controller'),
-	api = {
-		all: function(req, res, next) {
-			console.log(req.method, req.params, req.body, req.query);
-			next();
-		},
-		get: function(req, res) {
-			control.get({
+module.exports = function(io) {
 
-			}, function(err, ents) {
-				res.status(200).send(ents);
-			});
-		},
-		post: function(req, res) {
-			control.create({
+    var express = require('express'),
+        router = express.Router(),
+        control = require('../utils/controller'),
+        api = {
+            all: function(req, res, next) {
+                console.log(req.method, req.params, req.body, req.query);
+                next();
+            },
+            get: function(req, res) {
+                control.get({
 
-			}, function(err, ents) {
-				res.status(200).send(ents);
-			});
-		},
-		put: function(req, res) {
-			control.update({
+                }, function(err, ents) {
+                    res.status(200).send(ents);
+                });
+            },
+            post: function(req, res) {
+                control.create({
 
-			}, {
+                }, function(err, ents) {
+                    res.status(200).send(ents);
+                });
+            },
+            put: function(req, res) {
+                control.update({
 
-			}, {
-				upsert: true
-			}, function(err, affected, ents) {
-				res.status(200).send(ents);
-			});
-		},
-		delete: function(req, res) {
-			control.remove({
+                }, {
 
-			}, function(err, ents) {
-				res.status(204).send(ents);
-			});
-		},
-		merge: function(req, res) {
-			res.status(200).send('OK');
-		},
-		patch: function(req, res) {
-			res.status(200).send('OK');
-		},
-		copy: function(req, res) {
-			res.status(200).send('OK');
-		},
-		head: function(req, res) {
-			res.status(200).send('OK');
-		},
-		options: function(req, res) {
-			res.status(200).send('OK');
-		},
-		purge: function(req, res) {
-			res.status(200).send('OK');
-		},
-		lock: function(req, res) {
-			res.status(200).send('OK');
-		},
-		unlock: function(req, res) {
-			res.status(200).send('OK');
-		},
-		propfind: function(req, res) {
-			res.status(200).send('OK');
-		}
-	};
+                }, {
+                    upsert: true
+                }, function(err, affected, ents) {
+                    res.status(200).send(ents);
+                });
+            },
+            delete: function(req, res) {
+                control.remove({
 
-router
-	.all('/', api.all)
-	.get('/', api.get)
-	.post('/', api.post)
-	.put('/', api.put)
-	.delete('/', api.delete)
-	/*
-	.merge('/', api.merge)
-	.patch('/', api.patch)
-	.copy('/', api.copy)
-	.head('/', api.head)
-	.unlock('/', api.unlock)
-	.options('/', api.options)
-	.purge('/', api.purge)
-	.lock('/', api.lock)
-	.propfind('/', api.propfind)
-	*/
-	;
+                }, function(err, ents) {
+                    res.status(204).send(ents);
+                });
+            },
+            merge: function(req, res) {
+                res.status(200).send('OK');
+            },
+            patch: function(req, res) {
+                res.status(200).send('OK');
+            },
+            copy: function(req, res) {
+                res.status(200).send('OK');
+            },
+            head: function(req, res) {
+                res.status(200).send('OK');
+            },
+            options: function(req, res) {
+                res.status(200).send('OK');
+            },
+            purge: function(req, res) {
+                res.status(200).send('OK');
+            },
+            lock: function(req, res) {
+                res.status(200).send('OK');
+            },
+            unlock: function(req, res) {
+                res.status(200).send('OK');
+            },
+            propfind: function(req, res) {
+                res.status(200).send('OK');
+            }
+        };
 
-module.exports = router;
+    router
+        .all('/', api.all)
+        .get('/', api.get)
+        .post('/', api.post)
+        .put('/', api.put)
+        .delete('/', api.delete)
+        /*
+		.merge('/', api.merge)
+		.patch('/', api.patch)
+		.copy('/', api.copy)
+		.head('/', api.head)
+		.unlock('/', api.unlock)
+		.options('/', api.options)
+		.purge('/', api.purge)
+		.lock('/', api.lock)
+		.propfind('/', api.propfind)
+		*/
+	    ;
+
+    return router;
+}
